b4cbecc531f1a485a5bf68ec1d76ad1214517880
Kazi Shah Hamza <kazishahhamza@outlook.com>
2026-07-27 12:59:36 +0600

    updated file 47

--- 1. Arrays, Two Pointer, Sliding Window, Strings/test47.cpp
+++ 1. Arrays, Two Pointer, Sliding Window, Strings/test47.cpp
@@ -9,7 +9,16 @@ int main() {
     cout << endl;
     
     
+    int a = 50;
+    // int b = *p;
+    int *p = &a;
+    *p = 30;
     
+    cout << a << endl;
+    cout << &a << endl;
+    cout << p << endl;
+    cout << *p << endl;
+    // cout << b << endl;
     
     
     cout << endl;
